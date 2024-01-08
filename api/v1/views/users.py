9cb76031aaddaa92838423f14dffaf3e6d3958d2
Allan <allancheruiyot003@gmail.com>
2024-01-08 05:45:24 -0500

    ALX SE

---
+++ api/v1/views/users.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from api.v1.views import app_views
+from models import storage
+from models.user import User
+from flask import jsonify
+
+@app_views.route('/api/v1/users', methods=['GET'])
+def get_all_users():
+    users = storage.all(User)
+    return jsonify(users)
+@app_views.route('/api/v1/users/<user_id>', methods=['GET'])
+def get_user_by_id(user_id)
